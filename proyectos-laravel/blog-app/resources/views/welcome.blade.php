6d128dd285c263ac754c906e2cf73f0ffaa7c665
ABIU Condori <elicondoriquispe860@gmail.com>
2023-11-04 09:12:20 -0500

    Incluir css en la vista de Blade

--- proyectos-laravel/blog-app/resources/views/welcome.blade.php
+++ proyectos-laravel/blog-app/resources/views/welcome.blade.php
@@ -5,27 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog de Abiu</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <article>
-        <h1>Curso de Laravel 9/10</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
-        </p>
-    </article>
 
-    <article>
-        <h1>Novedades de Laravel 9/10</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
-        </p>
-    </article>
+    <div class="container">
+        <article>
+            <h1>Curso de Laravel 9/10</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
+            </p>
+        </article>
     
-    <article>
-        <h1>Manejo basico de Eloquent</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
-        </p>
-    </article>
+        <article>
+            <h1>Novedades de Laravel 9/10</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
+            </p>
+        </article>
+        
+        <article>
+            <h1>Manejo basico de Eloquent</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores, molestias nesciunt tempora pariatur doloremque reiciendis id explicabo tempore.
+            </p>
+        </article>
+    </div>
+    
 </body>
 </html>
