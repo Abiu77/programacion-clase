48820a79e708745e18a2fe507550020718410574
ABIU Condori <elicondoriquispe860@gmail.com>
2023-11-04 12:48:57 -0500

    Creación de Controladores con php artisan make para mantener el proyecto más organizado

--- proyectos-laravel/blog-app/routes/web.php
+++ proyectos-laravel/blog-app/routes/web.php
@@ -1,21 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 
-Route::get('/', function () {
-    $posts = [
-        [
-            'title' => 'Novedades de Laravel 9',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores'
-        ],
-        [
-            'title' => 'Curso de Laravel 9',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores'
-        ],
-        [
-            'title' => 'Manejo basico de Eloquent',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum cupiditate possimus corrupti accusamus assumenda, enim asperiores'
-        ],
-    ];
-    return view('welcome')->with('posts', $posts);
+Route::get('/', [HomeController::class, "show"]);
+
+Route::controller(PostsController::class)-> group(function(){
+    Route::get('/posts/{id}', "show");
+    Route::get('/posts/create', "create");
+    Route::post('/posts', "store");
+    Route::get('/posts/{id}/edit', "edit");
+    Route::patch('/posts/{id}/edit', "update");
+    Route::delete('/posts/{id}/edit', "destroy");
 });
