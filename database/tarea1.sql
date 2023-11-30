2b8a8c517ea43808ef88a66dd1a9443e2c5d6584
ABIU Condori <elicondoriquispe860@gmail.com>
2023-11-29 23:01:27 -0500

    Se añadio la entidad usuario

--- database/tarea1.sql
+++ database/tarea1.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE tarea1;
+
+USE tarea1;
+
+CREATE TABLE user (
+	iduser INT PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR (20),
+    email VARCHAR(20),
+    contraseña VARCHAR(20)
+)
