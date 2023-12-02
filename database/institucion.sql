125ca2657bb64fd34180c0eefd99871416ba9c26
Eli Abiu Condori Quispe <elicondoriquispe860@gmail.com>
2023-12-02 07:22:38 -0500

    se agrego la tabla grado

--- database/institucion.sql
+++ database/institucion.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE institucion;
 USE institucion;
+
+CREATE TABLE grado (
+	idgrado INT PRIMARY KEY AUTO_INCREMENT,
+    grado VARCHAR(45),
+    seccion VARCHAR(2),
+    tutor VARCHAR(75)
+);
