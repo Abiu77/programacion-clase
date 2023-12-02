41dc256e5668b69b3b65b74ca2af20b9d46e12a7
Eli Abiu Condori Quispe <elicondoriquispe860@gmail.com>
2023-12-02 08:42:42 -0500

    se agrego la tabla estudiante y asistencia

--- database/institucion.sql
+++ database/institucion.sql
@@ -7,3 +7,22 @@ CREATE TABLE grado (
     seccion VARCHAR(2),
     tutor VARCHAR(75)
 );
+
+CREATE TABLE estudiante (
+	idestudiante INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(75),
+    apellido VARCHAR(55),
+    dni INT,
+    codigo_est VARCHAR(20),
+    idgrado INT,
+    FOREIGN KEY (idgrado) REFERENCES grado (idgrado)
+);
+
+CREATE TABLE asistencia (
+	idasistencia INT PRIMARY KEY,
+    inasist_justificada VARCHAR(2),
+    inasist_injustificada VARCHAR(2),
+    tar_justificada VARCHAR(2),
+    idestudiante INT,
+    FOREIGN KEY(idestudiante) REFERENCES estudiante(idestudiante)
+);
