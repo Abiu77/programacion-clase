bc8536a2617f4ca3814b91bf80b094a01a20226b
Eli Abiu Condori Quispe <elicondoriquispe860@gmail.com>
2024-03-03 15:12:27 -0500

    Sintaxis JS

---
+++ JavaScript/JS-desde-Cero/scripts.js
@@ -0,0 +1,51 @@
+/**
+ * TIPOS DE DATOS - Coleccion de datos
+ */
+
+// ARRAY (Arreglo)
+// y = ['raul', 'juan', 'pepe'];
+
+// console.log(y[0]);
+
+// OBJETOS
+alumno = {
+  nombre: "Juan",
+  apellido: "Condori"
+};
+
+/**
+ * TIPOS DE DATOS - No definido
+ */
+
+// console.log(typeof undefined);
+
+/**
+ * VARIABLES
+ */
+
+// declaracion
+let persona;
+
+// Asignacion
+persona = 'Juan Perez';
+persona = 'Pablito';
+
+// console.log(persona);
+
+/**
+ * ASIGNACION POR REFERENCIA
+ */
+
+let profesor = 'Alexis';
+let alumno1 = profesor;
+
+profesor = 'Alvaro';
+
+// console.log(profesor);
+// console.log(alumno1);
+
+/**
+ * CONSTANTE
+ */
+
+const NOMBRE_EMPRESA = 'EDteam';
